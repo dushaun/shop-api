937a4848bd64484761e06fcd47c7cae3b2ad10d2
DuShaun Alderson-Claeys <dushaun.ac@gmail.com>
2019-01-02 13:43:31 +0000

    added laravel debugbar, allows insight into performance during request

--- app/Http/Controllers/Products/ProductController.php
+++ app/Http/Controllers/Products/ProductController.php
@@ -21,7 +21,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::withScopes($this->scopes())->paginate(10);
+        $products = Product::with(['variations.stock'])
+            ->withScopes($this->scopes())->paginate(10);
 
         return ProductIndexResource::collection($products);
     }
@@ -34,6 +35,8 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
+        $product->load(['variations.type', 'variations.stock', 'variations.product']);
+
         return new ProductResource($product);
     }
 
